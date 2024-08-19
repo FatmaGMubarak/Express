dad05863ed006ce37fb4b772fab97324d6def168
FatmaGMubarak <fatmagmoubarak@gmail.com>
2024-08-19 21:02:15 +0300

    dRequest

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-
-const express = require ("express")
+const fs = require("fs")
+const express = require ("express");
+const { error } = require('console');
 const app = express();
 const port = process.env.PORT;
 app.listen(port , ()=>console.log(`Server is running on port ${port}`));
@@ -11,20 +12,23 @@ app.use(express.json());
 
 
 app.get("/" ,(req,res) =>{
-    res.status(200).json({msg:"Get Request"})
+    fs.readFile("database.json" ,"utf8" ,(err,data)=>{
+        if (err ) res.status (400).json ({err})
+        if (data ) res.status (200).json ({data : JSON.parse (data)})
+    })
 });
 
 
 
-app.post("/" ,(req,res) =>{
-    console.log(req.body)
-    res.status(201).json({msg:"Post Request"})
-});
+// app.post("/" ,(req,res) =>{
+//     console.log(req.body)
+//     res.status(201).json({msg:"Post Request"})
+// });
 
-app.put("/:id" ,(req,res) =>{
-    res.status(201).json({id :req.params.id})
-});
-app.delete("/:id" ,(req,res) =>{
-    console.log(req.body)
-    res.status(201).json({id :req.params.id})
-});
+// app.put("/:id" ,(req,res) =>{
+//     res.status(201).json({id :req.params.id})
+// });
+// app.delete("/:id" ,(req,res) =>{
+//     console.log(req.body)
+//     res.status(201).json({id :req.params.id})
+// });
